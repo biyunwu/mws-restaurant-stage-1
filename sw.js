1330415096ed8406e408fc5151ee6814814c9e36
Biyun Wu <beenwu2016@gmail.com>
2018-08-27 17:59:41 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,84 @@
+// The SW code is derived from and modified based on Google Developer documentation.
+// https://developers.google.com/web/fundamentals/primers/service-workers/
+
+var CACHE_NAME = 'restaurant';
+var urlsToCache = [
+    '/',
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/dbhelper.js',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function(response) {
+                // Check if we received a valid response
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                    });
+
+                return response;
+                }
+            );
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['restaurant'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+            cacheNames.map(function(cacheName) {
+                if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+                }
+            })
+            );
+        })
+    );
+});
